4c5fe9a6ce4b01a3d82a880629690ba5f2628935
Nickolasso_2 <nickgh668@gmail.com>
2023-03-30 19:34:55 +0200

    client minus

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8e#6$9(2xt)a*z#!2k7s#fa53181^ai(3$$z7($o7-)i2t@oq!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
